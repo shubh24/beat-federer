d2bd6f4274258b215ee13c8038e3f5a900db7c3e
shubh24 <shubhankar24@gmail.com>
2016-07-05 09:49:49 +0530

    probabilities

--- simulate.py
+++ simulate.py
@@ -14,24 +14,52 @@ class Match():
 		self.set_2 = "-1"  
 		self.set_3 = "-1"  
 		self.set_4 = "-1"  
-		self.set_5 = "-1"  
+		self.set_5 = "-1"
+		self.set_count = 1
+		self.server = 0  #Federer's Service
+		self.toggle = [1, 0]
 
-	def play_point(self, me, fed):
-		return fed if random.randint(0,1) == 0 else me
+		# fed_serve = 0.677*0.735 + 0.333*0.54
+		self.fed_serve = 0.99 + 0.01*(0.772 - 0.5)/.5  
+		# fed_return = 0.335
+		self.fed_return = 0.99 - 0.01*(0.5 - 0.335/0.5)
+
+
+	def play_point(self, me, fed, server):
+
+		ran = random.random()
+
+		if server == 0:
+			if ran <= self.fed_serve:
+				return fed
+			else:
+				print "yay"
+				return me
+		else:
+			if ran <= self.fed_return:
+				return fed
+			else:
+				print "yay"
+				return me
 
 	def play_tiebreak(self, me, fed):
 		me.points_won = 0
 		fed.points_won = 0
 		while(max(me.points_won, fed.points_won) < 7):
-			
+			if (me.points_won + fed.points_won)%2 == 1:
+				self.server = self.toggle[self.server] #Toggle server
+
 			if me.points_won == 5 and fed.points_won == 5:		
 				while(abs(me.points_won - fed.points_won) != 2):
-					diffof2_winner = self.play_point(me, fed)
+					diffof2_winner = self.play_point(me, fed, self.server)
 					diffof2_winner.points_won += 1				
-				return me if me.points_won > fed.points_won else fed
 
+					if (me.points_won + fed.points_won)%2 == 1:
+						self.server = self.toggle[self.server] #Toggle server
+
+				return me if me.points_won > fed.points_won else fed
 			else:
-				winner = self.play_point(me, fed)
+				winner = self.play_point(me, fed, self.server)
 				winner.points_won += 1
 		
 		return me if me.points_won > fed.points_won else fed 
@@ -39,24 +67,26 @@ class Match():
 	def play_game(self, me, fed):
 		me.points_won = 0
 		fed.points_won = 0
+
 		while(max(me.points_won, fed.points_won) < 4):
 			if me.points_won == 3 and fed.points_won == 3:
-				deuce_winner = self.play_point(me, fed)
-				ad_winner = self.play_point(me, fed)
+				deuce_winner = self.play_point(me, fed, self.server)
+				ad_winner = self.play_point(me, fed, self.server)
 				if deuce_winner.name == ad_winner.name:
 					ad_winner.points_won += 1					
 			else:
-				winner = self.play_point(me, fed)
+				winner = self.play_point(me, fed, self.server)
 				winner.points_won += 1
 
+		self.server = self.toggle[self.server] #Toggle server
 		return me if me.points_won > fed.points_won else fed 
 
-	def play_set(self, me, fed, set_count):
+	def play_set(self, me, fed):
 		me.games_won = 0
 		fed.games_won = 0
 		while(max(me.games_won, fed.games_won) < 6):
 			
-			if set_count != 5:			
+			if self.set_count != 5:			
 				if me.games_won == 5 and fed.games_won == 5:
 					fiveall_winner = self.play_game(me, fed)
 					fiveall_winner.games_won += 1
@@ -72,7 +102,7 @@ class Match():
 						tiebreak_winner.games_won += 1
 						return tiebreak_winner
 			
-			elif set_count == 5:
+			elif self.set_count == 5:
 				if me.games_won == 5 and fed.games_won == 5:
 					while(abs(me.games_won - fed.games_won) != 2):
 						diffof2_winner = self.play_game(me, fed)
@@ -85,14 +115,13 @@ class Match():
 		return me if me.games_won > fed.games_won else fed 
 
 	def play_match(self, me, fed):
-		set_count = 1
 		while(max(me.sets_won, fed.sets_won) < 3):
-			winner = self.play_set(me, fed, set_count)
+			winner = self.play_set(me, fed)
 			winner.sets_won += 1
 
-			var = "set_%s"%set_count
+			var = "set_%s"%self.set_count
 			self.__dict__[var] = "%s-%s"%(me.games_won, fed.games_won)
-			set_count += 1
+			self.set_count += 1
 
 		print self.set_1, self.set_2, self.set_3, self.set_4, self.set_5
 
